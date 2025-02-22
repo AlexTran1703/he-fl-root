359f845080e1f9faeba465482919acee0f1ddc94
Alex Tran <khanh1234tg@gmail.com>
2025-02-22 10:06:02 +0700

    v1 | beta

---
+++ scripts/clear_script.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+(rm -r /home/alex/dev/project-root/build/* /home/alex/dev/project-root/build/.* 2>/dev/null)
+(rm -r /home/alex/dev/project-root/build_release/* /home/alex/dev/project-root/build_release/.* 2>/dev/null)
